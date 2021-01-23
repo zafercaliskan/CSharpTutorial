cbd10b73e6ed65da0890f8bdf6b277d36261d44d
Zafer Çalışkan <40596204+zafercaliskan@users.noreply.github.com>
2021-01-24 01:04:47 +0300

    20 january course subjects (4th lesson) (#3)
    * add oop project
    
    * add Collections project
    
    * add GenericsIntro Project

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> names = new MyList<string>();
+            names.Add("Zafer");
+
+            Console.WriteLine($"Length: {names.Length}\n--------");
+            names.Add("Onur");
+
+            foreach (var name in names.Items)
+            {
+                Console.WriteLine($"Name: {name}");
+            }
+        }
+    }
+}
