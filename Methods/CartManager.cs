42c5e77949e2269f12a819a93f2e365401f144f3
Zafer Çalışkan <40596204+zafercaliskan@users.noreply.github.com>
2021-01-23 23:26:32 +0300

    add methods project

---
+++ Methods/CartManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    ////naming convention
+    class CartManager //CartService
+    {
+        //✓
+        public void Add(Product product)
+        {
+            Console.WriteLine("Congratulations. Added to cart: {0}", product.Name);
+        }
+
+        //×××
+        public void Add2(string productName, string description, double price, int stockQuantity)
+        {
+            Console.WriteLine("Congratulations. Added to cart: {0}", productName);
+        }
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] products = new string[] { "Dell", "Asus" };
+
+            Product product1 = new Product();
+            product1.Id = new Guid();
+            product1.Name = "Apple";
+            product1.Price = 15;
+            product1.StockQuantity = 100;
+
+            Product product2 = new Product()
+            {
+                Id = new Guid(),
+                Name = "Samsung",
+                Price = 20,
+                StockQuantity = 200
+            };
+
+            Product[] products = new Product[] { product1, product2 };
+
+            //type safe
+            foreach (var product in products)
+            {
+                Console.WriteLine("Name:{0}\nPrice:{1}\n--------");
+            }
+
+            //instance
+            CartManager cartManager = new CartManager();
+            
+            //encapsulation
+            cartManager.Add(product1);
+            cartManager.Add(product2);
+
+            cartManager.Add2("Asus", "Rog", 200, 50);
+            cartManager.Add2("Dell", "Inspiron", 150, 25);
+        }
+    }
+}
+
+//Dont repeat yourself - DRY - Clean Code - Best Practice
