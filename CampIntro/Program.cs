2ca273afe909d402d568633e7a5bd76a1000bcd2
Zafer Çalışkan <40596204+zafercaliskan@users.noreply.github.com>
2021-01-23 22:12:24 +0300

    update CampIntro project

--- CampIntro/Program.cs
+++ CampIntro/Program.cs
@@ -6,7 +6,37 @@ namespace CampIntro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Type safety
+            //Do not reapet yourself
+
+            string categoryLabel = "Category Label";
+            Console.WriteLine(categoryLabel);
+
+            double dollarYesterday = 7.55, dollarToday = 7.35;
+
+            bool loggedIntoTheSystem = true;
+
+            if (dollarYesterday > dollarToday)
+            {
+                Console.WriteLine("Decrease");
+            }
+            else if (dollarYesterday < dollarToday)
+            {
+                Console.WriteLine("Increase");
+            }
+            else
+            {
+                Console.WriteLine("constant");
+            }
+
+            if (loggedIntoTheSystem) //loggedIntoTheSystem == true
+            {
+                Console.WriteLine("Dashboard");
+            }
+            else
+            {
+                Console.WriteLine("Login screen");
+            }
         }
     }
 }
