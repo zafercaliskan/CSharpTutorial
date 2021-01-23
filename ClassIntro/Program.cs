a7d46f01762b31dcf9a6eecef9d2c7e061251b74
Zafer Çalışkan <40596204+zafercaliskan@users.noreply.github.com>
2021-01-23 22:32:26 +0300

    13 january course subjects (#1)
    * update CampIntro project
    
    * add loops project
    
    * add CampIntro project

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Course course1 = new Course();
+            course1.CourseName = "C#";
+            course1.Instructor = "Engin Demiroğ";
+            course1.ViewRate = 95;
+
+            //Or
+
+            Course course2 = new Course()
+            {
+                CourseName = "Selenium",
+                Instructor = "Zafer Çalışkan",
+                ViewRate = 90
+            };
+
+            Course[] courses = new Course[] { course1, course2 };
+
+            foreach (Course course in courses) //(var course in courses) 
+            {
+                Console.WriteLine("Course Name:{0}\nCourse Instructor: {1}\nView Rate: {2}\n\n", course.CourseName, course.Instructor, course.ViewRate);
+            }
+        }
+    }
+
+    class Course
+    {
+        public string CourseName { get; set; }
+        public string Instructor { get; set; }
+        public int ViewRate { get; set; }
+    }
+}
---
+++ Loops/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string course1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string course2 = "Programlamaya başlangıç için temel kurs";
+            string course3 = "Java";
+            string course4 = "Python";
+            string course5 = "C++";
+
+            Console.WriteLine(course1);
+            Console.WriteLine(course2);
+            Console.WriteLine(course3);
+            Console.WriteLine(course4);
+            Console.WriteLine(course5);
+
+            //Array
+            string[] courses = new string[] { course1, "Programlamaya başlangıç için temel kurs", "Java", "Python", "C#" };
+
+            for (int i = 0; i < courses.Length; i++)
+            {
+                Console.WriteLine(courses[i]);
+            }
+
+            Console.WriteLine("-----");
+
+            foreach (var course in courses)
+            {
+                Console.WriteLine(course);
+            }
+
+            Console.WriteLine("-----");
+        }
+    }
+}
