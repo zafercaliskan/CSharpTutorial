709521476a93a1d784f8e42567a8441f22e81938
Zafer Çalışkan <40596204+zafercaliskan@users.noreply.github.com>
2021-01-23 20:01:01 +0300

    Add project files.

---
+++ CampIntro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
