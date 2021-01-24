f405f698ed8a3d03b01de78a6ed48c578e855c16
Zafer Çalışkan <40596204+zafercaliskan@users.noreply.github.com>
2021-01-24 16:06:57 +0300

    2nd day homework

---
+++ ThirteenJanuary/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ThirteenJanuary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Asus ROG Strix G17",
+                Category = "Laptop",
+                Price = 13999
+            };
+
+            Product product2 = new Product()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Xiaomi Mi 10T Pro",
+                Category = "Phone",
+                Price = 5799
+            };
+
+            List<Product> products = new List<Product>() { product1, product2 };
+
+            login();
+
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Id:{product.Id} Name:{product.Name} Category:{product.Category} Price: {product.Price} TL");
+            }
+        }
+
+        static void login()
+        {
+            //string userId = "zafercaliskan", password = "123456";
+            Console.Write("Id:");
+            string userId = Console.ReadLine();
+
+            if (userId == "zafercaliskan")
+            {
+                Console.Write("Password:");
+                string password = Console.ReadLine();
+                if (password == "123456")
+                {
+                    Console.WriteLine("You have successfully logged into the system.");
+                }
+                else
+                {
+                    ExitProgram("Incorrect password.");
+                }
+            }
+            else
+            {
+                ExitProgram("Incorrect user id");
+            }
+        }
+        static void ExitProgram(string errorMessage)
+        {
+            Console.WriteLine(errorMessage);
+            System.Environment.Exit(1);
+        }
+    }
+}
