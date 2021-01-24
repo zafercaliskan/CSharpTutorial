d108b140ae3679ea9838ced598fb0a897abd27b1
Zafer Çalışkan <40596204+zafercaliskan@users.noreply.github.com>
2021-01-24 19:30:26 +0300

    add RefOut Project

---
+++ RefOut/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RefOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 10; //required (ref)
+            int number2 = 50;
+            int number3; //not required (out)
+
+
+            var result = math(ref number1, number2);
+            Console.WriteLine($"Result: {result}");
+            Console.WriteLine($"Number1: {number1}");
+
+        }
+        static int math(ref int number1, int number2)
+        {
+            number1 += 5;
+            //number1 += 5; //not required
+            return number1 + number2;
+        }
+        static int math2(out int number3, int number2)
+        {
+            //required
+            number3 = 5;
+            return number3 + number2;
+        }
+
+    }
+}
