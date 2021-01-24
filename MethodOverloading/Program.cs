1788087f0ca16f369889b637225153b168277770
Zafer Çalışkan <40596204+zafercaliskan@users.noreply.github.com>
2021-01-24 19:41:46 +0300

    add MethodOverloading project (#7)

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static int PlusMethod(int x, int y)
+        {
+            return x + y;
+        }
+
+        static double PlusMethod(double x, double y)
+        {
+            return x + y;
+        }
+        static void Main(string[] args)
+        {
+            int number1 = PlusMethod(10, 5);
+            double number2 = PlusMethod(9.5, 7.23);
+
+            Console.WriteLine($"Int: {number1} Double: {number2}");
+        }
+    }
+}
