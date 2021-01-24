2a38c9dc4ad50da8ec6f19b2eda813a315b72879
Zafer Çalışkan <40596204+zafercaliskan@users.noreply.github.com>
2021-01-24 16:09:47 +0300

    2nd day homework (#4)

---
+++ ThirteenJanuary/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThirteenJanuary
+{
+    class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Price { get; set; }
+    }
+}
