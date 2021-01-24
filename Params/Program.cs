31d7c3be904ced153108456e337275e0c01c1c50
Zafer Çalışkan <40596204+zafercaliskan@users.noreply.github.com>
2021-01-24 19:50:04 +0300

    add Params Project

---
+++ Params/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Params
+{
+    class Program
+    {
+        static int Math(params int[] numbers)
+        {
+            return numbers.Sum();
+        }
+        static void Main(string[] args)
+        {
+            var result = Math(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+            Console.WriteLine($"Result {result}");
+        }
+    }
+}
