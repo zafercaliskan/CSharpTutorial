2ea36c694b0cf38be0795732756945aaf13e3ad0
Zafer Çalışkan <40596204+zafercaliskan@users.noreply.github.com>
2021-01-31 17:08:39 +0300

    add RecapDemo project

---
+++ RecapDemo/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecapDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button[,] buttons = new Button[8, 8];
+            //GetUpperBound(0) boyutun en fazla alabieleceği değer.
+
+            int top = 0;
+            int left = 0;
+
+            for (int i = 0; i < buttons.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j < buttons.GetUpperBound(1); j++)
+                {
+                    buttons[i, j] = new Button();
+                    buttons[i, j].Width = 50;
+                    buttons[i, j].Height = 50;
+                    buttons[i, j].Top = top;
+                    buttons[i, j].Left = left;
+                    this.Controls.Add(buttons[i, j]);
+                    left += 50;
+                    if ((i + j) % 2 == 0)
+                    {
+                        buttons[i, j].BackColor = Color.Black;
+                    }
+                    else
+                    {
+                        buttons[i, j].BackColor = Color.White;
+                    }
+                }
+                top += 50;
+                left = 0;
+            }
+
+        }
+    }
+}
