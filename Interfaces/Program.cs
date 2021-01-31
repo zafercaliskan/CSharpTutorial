a5267b3e1db13535db0a2405e452ed0c1f6375fd
Zafer Çalışkan <40596204+zafercaliskan@users.noreply.github.com>
2021-01-31 21:49:59 +0300

    Updated Interfaces Project

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -11,8 +11,7 @@ namespace Interfaces
             //IPersonManager personManager = new IPersonManager();
 
             //IPersonManager customerManager = new CustomerManager(); //Interfaceler referans tiptir. Heap'de CustomerManager'ın bir referans değeri oluşuyor.
-            //IPersonManager'da bu referans değerini tutabiliyor. Yani interfaceler onu implemente eden (new CustomerManager();) class'ın 
-            //referans numarasını tutabilirler.
+            //IPersonManager'da bu referans değerini tutabiliyor. Yani interfaceler onu implemente eden (new CustomerManager();) class'ın referans numarasını tutabilirler.
 
             IPersonManager customerManager = new CustomerManager(); //CustomerManager referansını tutar.
             customerManager.Add();
@@ -33,7 +32,6 @@ namespace Interfaces
     //class CustomerManager : PersonManager
     //{
     //    //CustomerManager kendi operasyonlarına ek olarak kalıtım aldığı base sınıfın operasyonlarını kullanabilir dedik.
-
     //}
 
     interface IPersonManager //Şablon belirliyoruz.  void Add() imzasını kullanmak zorunda fakat kendi kodlarını yazabilir.
@@ -44,7 +42,7 @@ namespace Interfaces
     }
 
     //inherits-class ------ implements-interface
-    class CustomerManager : IPersonManager //Bunlarda bu şablona uymak zorunda. İmzalar aynı. Kod içerikleri farklı. Buda IPersonManager.
+    class CustomerManager : IPersonManager //Bunlarda şablona uymak zorunda. İmzalar aynı. Kod içerikleri farklı. Buda IPersonManager.
     {
         public void Add() //Burada imzalar aynı fakat içerisindeki kodlar farklıdır.
         {
@@ -58,7 +56,7 @@ namespace Interfaces
         }
     }
 
-    class EmployeeManager : IPersonManager //Bunlarda bu şablona uymak zorunda. İmzalar aynı. Kod içerikleri farklı. Buda IPersonManager.
+    class EmployeeManager : IPersonManager //Bunlarda şablona uymak zorunda. İmzalar aynı. Kod içerikleri farklı. Buda IPersonManager.
     {
         public void Add()
         {
@@ -102,7 +100,7 @@ namespace Interfaces
         //Parametre olarak interface alır. Buraya IPersonManager'dan implement edilen tüm class'lar gönderilebilir.
         //Burası IPersonManager kabul ediyor. Oda IPersonManager'dan implement edilen tüm sınıfları kabul eder.
         //Bu bize yazılımda değişimi sağlar. Yazılımda çok temel prensip yeni özellik eklediğinizde mevcut kodlara dokunulmaz.
-        //CustomerManager ve EmployeeManager varken InternManager ekleyip IPersonManager'dan implement ederiz kendi operasyon kodlarını yazarız (Add, Update)
+        //CustomerManager ve EmployeeManager varken InternManager ekleyip IPersonManager'dan implement ederiz kendi operasyon kodlarını yazarız (Add, Update).
         //Fakat ProjectManager için birşey değiştirmemiz gerekmez çünkü IPersonManager kabul ediyor. InternManager'da IPersonManager'dan implement edildiği 
         //için kabul edecektir.
 
