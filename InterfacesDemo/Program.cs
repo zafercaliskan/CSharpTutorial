6601a7d8e0c56ec9a2d53203548529ba70e5a426
Zafer Çalışkan <40596204+zafercaliskan@users.noreply.github.com>
2021-01-31 17:56:53 +0300

    add InterfacesDemo project

---
+++ InterfacesDemo/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace InterfacesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWorker[] workers = new IWorker[3]
+            {
+                new Manager(),
+                new Worker(),
+                new Robot()
+            };
+
+            IEat[] eats = new IEat[2]
+            {
+                new Manager(),
+                new Worker()
+            };
+        }
+    }
+
+    //SOLID Interface Segregation
+    interface IWorker
+    {
+        void Work();
+    }
+
+    interface IEat
+    {
+        void Eat();
+    }
+
+    interface ISalary
+    {
+        void GetSalary();
+    }
+
+    //Classlardan birden fazla interface'i implemente edebilir. 
+    class Manager : IWorker, IEat, ISalary
+    {
+        void IWorker.Work()
+        {
+            throw new NotImplementedException();
+        }
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetSalary()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Worker : IWorker, IEat, ISalary
+    {
+        void IWorker.Work()
+        {
+            throw new NotImplementedException();
+        }
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetSalary()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Robot : IWorker
+    {
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
