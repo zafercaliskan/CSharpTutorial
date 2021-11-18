fe50657efa904ad6c23ba921f74b3b8896a436e8
Zafer Çalışkan <40596204+zafercaliskan@users.noreply.github.com>
2021-11-19 00:05:44 +0300

    Add AccessModifiers Project

---
+++ AccessModifiers/CourseManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccessModifiers
+{
+    class CourseManager
+    {
+        public void Add()
+        {
+            Course course = new Course();
+            //Referans'a ihtiyac duymadan başka dosyada ama aynı projedeki class'a erişebilirz.
+            Console.WriteLine(course.Name);
+        }
+    }
+}
---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Customer
+    {
+        int Id; //default private'dır.
+
+        protected int FirstName;
+    }
+
+    class Student : Customer
+    {
+        public void Save()
+        {
+            //Burada firstname erişebiliriz.
+            Customer customer = new Customer();
+            //customer.id = 1; burada id gelmez. Çünkü private tanımlandığı blakda geçerlidir.
+        }
+    }
+
+    class Course //default internal
+    {
+        //private class NestedClass iç classda olur ama dış classda private yada protected olmaz.
+        //{
+
+        //}
+
+        //protected class NestedClass
+        //{
+
+        //}
+        public string Name { get; set; }
+    }
+
+    //Private: Tanımlandığı blok içerisinde geçerlidir. Class, field, property etc. olabilir.
+
+    //Protected: Private'ın tüm özelliklerini karşılar. Protected'ı class seviyesinde kullanırız.
+    //- Ayrıca artısı özellik değişken her neyse inherit edildiği sınıflarda da kullanılabilir.
+
+    //Internal: Bulunduğu proje içerisinde referans ihtiyacı olmadan kullanılabilir. Proje içerisinde heryerden çağırabilir..
+    //- iç içe class dışında private yada protected olamaz.
+
+    //Public: Course class'ını başka bir projeden de kullanmak istersek public yaparak ve referans vererek kullanabiliriz.
+}
