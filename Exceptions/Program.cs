0e5c551f0b54346748f1eb1eb60caf26e9cf6804
Zafer Çalışkan <40596204+zafercaliskan@users.noreply.github.com>
2021-11-19 00:24:23 +0300

    Use Func

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -11,10 +11,29 @@ namespace Exceptions
             //ExceptionIntro();
             //CustomRecordNotFoundException();
 
-            GlobalHandleException();
+            //ActionDemo();
+
+            Func<int, int, int> add = Topla;
+            Console.WriteLine(add(3, 5));
+
+            Func<int> getRandomNumber = delegate ()
+            {
+                Random random = new Random();
+                return random.Next(1, 100);
+            };
+
+            Func<int> getRandomNumber2 = () => new Random().Next(1, 100);
+            Console.WriteLine(getRandomNumber());
+            Thread.Sleep(1000);
+            Console.WriteLine(getRandomNumber2());
         }
 
-        private static void GlobalHandleException()
+        static int Topla(int x, int y)
+        {
+            return x + y;
+        }
+
+        private static void ActionDemo()
         {
             //Method gönderiyoruz delegeler yardımıyla.
             //HandleException(() =>  //'()'-> Parametresiz method göndereceğim.'=>' -> karşılığıda ( => bunada lamda denir) '{}' -> kod kümesi
