7eb00ffe2ffb90f12986ea4f5583dc33c634b00d
Zafer Çalışkan <40596204+zafercaliskan@users.noreply.github.com>
2021-11-18 23:54:50 +0300

    add VirtualMethods Project

---
+++ VirtualMethods/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace VirtualMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        class Database
+        {
+            public virtual void Add() //Virtual keyword'u ile Add methodunun inhertance yapılan sınıflarda
+                                      //override edilebileceğini belirttik. Isterse Base'deki Add methodunu override edebilir.
+            {
+                Console.WriteLine("Added by default");
+            }
+            public void Delete()
+            {
+                Console.WriteLine("Deleted by default");
+            }
+        }
+
+        class SqlServer : Database
+        {
+            //Bunu interfaceler ile yapamayız. inhertance kullanmamızı gerektiren bir durum.
+            public override void Add()
+            {
+                Console.WriteLine("Added by Sql code");  //İkisini de aynı anda (base dahil) çalıştırabiliriz.
+                                                         //Yada sadece buraya özgü birşeylerde yazabiliriz.
+
+                //Örneğin önden bir konfigürasyon çalıştırıp base'i de çalıştırabiliriz.
+                base.Add(); //Base'deki default add'i çalıştırır.
+            }
+        }
+        class MySql : Database
+        {
+
+        }
+    }
+}
